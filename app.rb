d5c581b11f8217826af6b43726f431462ce4dc40
Roberto Dip <dip.jesusr@gmail.com>
2014-08-29 22:16:20 -0300

    Add logic to load the application files in the app.rb file

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
+require "bundler"
 require "cuba"
 require "rack/protection"
+require "mongoid"
 
 Cuba.use Rack::Session::Cookie, key: ENV["APP_KEY"], secret: ENV["APP_SECRET"]
 Cuba.use Rack::Protection::RemoteReferrer
 Cuba.use Rack::MethodOverride
 Cuba.use Rack::Protection
+
+# load database configuration
+Mongoid.load!("config/mongoid.yml")
+
+# require application files
+Dir["./app/helpers/**/*.rb"].each { |f| require(f) }
+Dir["./app/models/**/*.rb"].each  { |f| require(f) }
+Dir["./app/routes/**/*.rb"].each  { |f| require(f) }
